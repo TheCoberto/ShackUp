09433b33d614d62c7374d52af2d47024af6b26a7
wkelly <coby.kelly.ck@gmail.com>
2024-05-03 20:12:12 -0400

    Add project files.

---
+++ ShackUp.Data/ADO/BathroomTypesRepositoryADO.cs
@@ -0,0 +1,42 @@
+﻿using ShackUp.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShackUp.Models.Tables;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ShackUp.Data.ADO
+{
+    public class BathroomTypesRepositoryADO : IBathroomTypesRepository
+    {
+        public List<BathroomType> GetAll()
+        {
+            List<BathroomType> bathroomTypes = new List<BathroomType>();
+
+            using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            {
+                SqlCommand cmd = new SqlCommand("BathroomTypesSelectAll", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        BathroomType currentRow = new BathroomType();
+                        currentRow.BathroomTypeId = (int)dr["BathroomTypeId"];
+                        currentRow.BathroomTypeName = dr["BathroomTypeName"].ToString();
+
+                        bathroomTypes.Add(currentRow);
+                    }
+                }
+            }
+
+            return bathroomTypes;
+        }
+    }
+}
---
+++ ShackUp.Data/ADO/StatesRepositoryADO.cs
@@ -0,0 +1,42 @@
+﻿using ShackUp.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShackUp.Models.Tables;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ShackUp.Data.ADO
+{
+    public class StatesRepositoryADO : IStatesRepository
+    {
+        public List<State> GetAll()
+        {
+            List<State> states = new List<State>();
+
+            using (var cn = new SqlConnection(Settings.GetConnectionString()))
+            {
+                SqlCommand cmd = new SqlCommand("StatesSelectAll", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while(dr.Read())
+                    {
+                        State currentRow = new State();
+                        currentRow.StateId = dr["StateId"].ToString();
+                        currentRow.StateName = dr["StateName"].ToString();
+
+                        states.Add(currentRow);
+                    }                  
+                }
+            }
+
+            return states;
+        }
+    }
+}
